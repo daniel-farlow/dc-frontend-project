85a725b531c3ba16b49ab95ff56e25f48fd2fb46
Reuben Remy <reubenremy@gmail.com>
2019-10-03 11:33:05 -0400

    all items

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,156 @@
-$(function(){
-var loading = $('#loadbar').hide();
-$(document)
-.ajaxStart(function () {
-    loading.show();
-}).ajaxStop(function () {
-    loading.hide();
-});
+'use strict';
 
-$("label.btn").on('click',function () {
-    var choice = $(this).find('input:radio').val();
-    $('#loadbar').show();
-    $('#quiz').fadeOut();
-    setTimeout(function(){
-        $( "#answer" ).html(  $(this).checking(choice) );      
-        $('#quiz').show();
-        $('#loadbar').fadeOut();
-        /* something else */
-    }, 1500);
-});
+/*
+Sample JSON response:
+*/
 
-$ans = 3;
+const sampleResponse = {
+    "response_code": 0,
+    "results": [
+        {
+            "category": "Sports",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which of these teams isn&#039;t a member of the NHL&#039;s &quot;Original Six&quot; era?",
+            "correct_answer": "Philadelphia Flyers",
+            "incorrect_answers": [
+                "New York Rangers",
+                "Toronto Maple Leafs",
+                "Boston Bruins"
+            ]
+        },
+        {
+            "category": "Sports",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In a game of snooker, what colour ball is worth 3 points?",
+            "correct_answer": "Green",
+            "incorrect_answers": [
+                "Yellow",
+                "Brown",
+                "Blue"
+            ]
+        },
+        {
+            "category": "Sports",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Who won the 2015 Formula 1 World Championship?",
+            "correct_answer": "Lewis Hamilton",
+            "incorrect_answers": [
+                "Nico Rosberg",
+                "Sebastian Vettel",
+                "Jenson Button"
+            ]
+        },
+        {
+            "category": "Sports",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Josh Mansour is part of what NRL team?",
+            "correct_answer": "Penrith Panthers",
+            "incorrect_answers": [
+                "Melbourne Storm",
+                "Sydney Roosters",
+                "North Queensland Cowboys"
+            ]
+        },
+        {
+            "category": "Sports",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Which male player won the gold medal of table tennis singles in 2016 Olympics Games?",
+            "correct_answer": "Ma Long (China)",
+            "incorrect_answers": [
+                "Zhang Jike (China)",
+                "Jun Mizutani (Japan)",
+                "Vladimir Samsonov (Belarus)"
+            ]
+        },
+        {
+            "category": "Sports",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which soccer team won the Copa Am&eacute;rica 2015 Championship ?",
+            "correct_answer": "Chile",
+            "incorrect_answers": [
+                "Argentina",
+                "Brazil",
+                "Paraguay"
+            ]
+        },
+        {
+            "category": "Sports",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "In Baseball, how many times does the ball have to be pitched outside of the strike zone before the batter is walked?",
+            "correct_answer": "4",
+            "incorrect_answers": [
+                "1",
+                "2",
+                "3"
+            ]
+        },
+        {
+            "category": "Sports",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In 2016, who won the Formula 1 World Constructor&#039;s Championship for the third time in a row?",
+            "correct_answer": "Mercedes-AMG Petronas",
+            "incorrect_answers": [
+                "Scuderia Ferrari",
+                "McLaren Honda",
+                "Red Bull Racing Renault"
+            ]
+        },
+        {
+            "category": "Sports",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What is the oldest team in Major League Baseball?",
+            "correct_answer": "Atlanta Braves",
+            "incorrect_answers": [
+                "Chicago Cubs",
+                "Cincinnati Reds",
+                "St. Louis Cardinals"
+            ]
+        },
+        {
+            "category": "Sports",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Which year was the third Super Bowl held?",
+            "correct_answer": "1969",
+            "incorrect_answers": [
+                "1968",
+                "1971",
+                "1970"
+            ]
+        }
+    ]
+}
 
-$.fn.checking = function(ck) {
-    if (ck != $ans)
-        return 'INCORRECT';
-    else 
-        return 'CORRECT';
-}; 
-});	
+// SAMPLE RESPONSE OBJECT IS ABOVE
+
+const sportsCard = document.querySelector('#sports');
+const sportsCardQuestions = document.querySelector('.questions')
+const sportsCardQuestionsList = document.querySelector('.question-list')
+sportsCard.addEventListener('click', () => {
+    let allCards = document.querySelectorAll('.card');
+    for (let i = 0; i < allCards.length; ++i) {
+        allCards[i].classList.add('hide');
+     }
+    const questionTitle = document.createElement('h1');
+    questionTitle.innerHTML = sampleResponse.results[0].question;
+    sportsCardQuestions.prepend(questionTitle);
+    let categoryQuestions = [];
+    categoryQuestions.push(sampleResponse.results[0].correct_answer);
+    sampleResponse.results[0].incorrect_answers.forEach(answer => {
+        categoryQuestions.push(answer);
+    })
+    categoryQuestions.forEach(question => {
+        let questionItem = document.createElement('li');
+        questionItem.innerHTML = question;
+        sportsCardQuestionsList.append(questionItem)
+    })
+})
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,6 +1,13 @@
-// function for fetching data from an api
 function get(url) {
     return fetch(url)
         .then(response => response.json())
         .then(data => data);
+}
+
+
+function getQuestions(categoryNum) {
+    get(`https://opentdb.com/api.php?amount=10&category=${categoryNum}`)
+        .then(data => {
+            console.log(data.results);
+        })
 }
